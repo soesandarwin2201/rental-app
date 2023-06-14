cd45f75a2d5698825591f454f4afc78dcc5a097b
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-14 09:21:03 +0630

    Create the validation for the house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,15 @@
 class House < ApplicationRecord
+     validates :name, presence: true, length: { minimum: 3, message: 'Name must have 3 or more characters' }
+     validates :bedrooms, presence: true, allow_blank: true
+     validates :bathrooms, presence: true, allow_blank: true
+     validates :country, presence: true, allow_blank: true
+     validates :house_status, presence: true
+     validates :latitude, presence: true
+     validates :longitude, presence: true
+     validates :price,
+            presence: true,
+            numericality: { greater_than: 0, message: 'Price must be greater than 0' }
+     validates :update_date, presence: true
      has_many :images, dependent: :destroy
      has_many :addresses, dependent: :destroy
      has_many :attributions, dependent: :destroy
