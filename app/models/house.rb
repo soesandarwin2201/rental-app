66f99cb35eb09701623bbfb1295b8fdfca901a71
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-14 09:23:30 +0630

    Create the validation for the address and image

--- app/models/house.rb
+++ app/models/house.rb
@@ -6,10 +6,12 @@ class House < ApplicationRecord
      validates :house_status, presence: true
      validates :latitude, presence: true
      validates :longitude, presence: true
-     validates :price,
-            presence: true,
-            numericality: { greater_than: 0, message: 'Price must be greater than 0' }
+     validates :price, presence: true,
+                numericality: { greater_than: 0, message: 'Price must be greater than 0' }
      validates :update_date, presence: true
+     validates :detail, presence: true, allow_blank: true,
+                length: { maximum: 1000, message: 'Description must have less than 1000 characters' }
+     validates :square, presence: true
      has_many :images, dependent: :destroy
      has_many :addresses, dependent: :destroy
      has_many :attributions, dependent: :destroy
