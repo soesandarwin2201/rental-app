5be0c704748507d5443da6fad039e8905771ed92
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-14 09:30:24 +0630

    Create the validation for the agents

--- app/models/attribution.rb
+++ app/models/attribution.rb
@@ -1,3 +1,8 @@
 class Attribution < ApplicationRecord
+  validates :phone, presence: true, allow_blank: false
+  validates :name, presence: true, length: { minimum: 3, message: 'Name must have 3 or more characters' }
+  validates :company, presence: true, allow_blank: false
+  validates :facebook, presence: true, allow_blank: true
+  validates :address, presence: true, allow_blank: false
   belongs_to :house
 end
