847afcf42513b2191669e5930ebe983283a22d78
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-16 10:13:09 +0630

    Add the validation for the details and companyname of the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true, length: { in: 3..250, message: 'Must be a string of at least 3 characters' }
+  validates :name, presence: true, length: { in: 3..250, message: 'Name must be a string of at least 3 characters' }
   validates :email, presence: true, uniqueness: true
+  validates :company_name, presence: true, length: { in: 3..250, message: 'Comapany Name must be  least 3 characters' }
+  validates :address, presence: true
+  validates :detail, presence: true, allow_blank: true,
+                length: { maximum: 1500, message: 'Description must have less than 1500 characters' }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
