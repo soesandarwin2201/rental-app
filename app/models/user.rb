affc36f8daad343daaa72a843e4b2b3e514e1a3c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-16 09:46:08 +0630

    Create the validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: { in: 3..250, message: 'Must be a string of at least 3 characters' }
+  validates :email, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+
+  
 end
